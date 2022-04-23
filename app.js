997402e2aeb46ebf4e20f619ac6409e8201da2ee
Davide Merli <davide.merli@outlook.com>
2022-04-23 20:16:47 +0200

    fix: now changing cookies correctly

--- app.js
+++ app.js
@@ -1,40 +1,34 @@
-var createError = require('http-errors');
-var cors = require('cors');
-var express = require('express');
-var logger = require('morgan');
-var { createProxyMiddleware } = require('http-proxy-middleware');
-var cookieSession = require("cookie-session");
+var cors = require("cors");
+var express = require("express");
+var logger = require("morgan");
+var { createProxyMiddleware } = require("http-proxy-middleware");
 
 var app = express();
 
-app.set('trust proxy', 1)
 app.use(
-    cookieSession({
-      name: "__session",
-      keys: ["key1"],
-        maxAge: 24 * 60 * 60 * 100,
-        secure: true,
-        httpOnly: true,
-        sameSite: 'none'
-    })
+  cors({
+    credentials: true,
+    origin: "https://white-sky-04a01bf03.azurestaticapps.net",
+    origin: "http://20.113.77.53",
+  })
 );
 
-app.use(cors(
-  {
-    credentials:true,
-    origin: "https://white-sky-04a01bf03.azurestaticapps.net",
-    origin: "http://20.113.77.53"
-  }
-));
-
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 // proxy all requests to backend
 app.use(
   "",
   createProxyMiddleware({
-    target: 'http://20.113.77.53:8000', // target host
-    changeOrigin: true
+    target: "http://20.113.77.53:8000", // target host
+    changeOrigin: true,
+    onProxyRes(proxyRes, req, res) {
+      // change set-cookie into SameSite: None
+      if (proxyRes.headers["set-cookie"]) {
+        proxyRes.headers["set-cookie"] = proxyRes.headers["set-cookie"].map(
+          (cookie) => cookie + "; SameSite=None"
+        );
+      }
+    },
   })
 );
 
